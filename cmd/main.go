db5d18e2d8dab05fe28e7a85aca2521b968b0189
jcreixell <jorge.creixell@grafana.com>
2023-12-21 16:13:47 +0100

    Implement basic syntax with cross component references
      - References must be defined before they are used (sequential order)
      - Allows arbitrary export values
      - Seems like dots cannot be use in variable identifiers

--- cmd/main.go
+++ cmd/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"log"
+	"reflect"
 
 	"github.com/hashicorp/hcl2/gohcl"
 	"github.com/hashicorp/hcl2/hcl"
@@ -11,106 +12,148 @@ import (
 	"github.com/zclconf/go-cty/cty/gocty"
 )
 
+var testConfig = `
+component1 "yo" {
+	enabled = true
+}
+
+component1 "yo2" {
+	enabled = component1_yo_exports_enabled
+}
+`
+
 // Configuration is the top-level struct that contains attributes and blocks defined within
 // an `hcl` file.
 type Configuration struct {
-	Version string            `hcl:"version" cty:"version"`
-	Tags    map[string]string `hcl:"tags" cty:"tags"`
-
-	// Users refer to the `user` blocks defined in the `hcl` file
-	Users  []User   `hcl:"user,block" cty:"users"`
-	Yo     Yo       `hcl:"yo,block" cty:"yo"`
 	Remain hcl.Body `hcl:",remain"`
 }
 
-type PostConfiguration struct {
-	Pepe string `hcl:"pepe"`
+type ComponentNode struct {
+	ID            string
+	ComponentType reflect.Type
+	ConfigType    reflect.Type
+	Schema        hcl.BodySchema
 }
 
-// User is a hcl block containing information about a user. Unless specified, all fields
-// are required. In order to indicate that a field is optional, use the `optional` tag or
-// define the field as a pointer
-type User struct {
-	Username       string   `hcl:"username" cty:"username"`
-	FirstName      string   `hcl:"first_name" cty:"first_name"`
-	LastName       string   `hcl:"last_name" cty:"last_name"`
-	CloudProviders []string `hcl:"cloud_providers,optional" cty:"cloud_providers,optional"` // Example of optional field
-	Enabled        bool     `hcl:"enabled" cty:"enabled"`
+type ComponentConfig interface{}
+
+type Component interface {
+	GetID() string
+	Update(string, ComponentConfig)
+	Run()
+	Exports() map[string]cty.Value
 }
 
-type Yo struct {
-	Type       string `hcl:"type,label" cty:"type"`
-	Identifier string `hcl:"name,label" cty:"identifier"`
-	Mama       string `hcl:"mama" cty:"mama"`
+type Component1Config struct {
+	Enabled bool `hcl:"enabled,optional" cty:"enabled"`
 }
 
-// String prints out a pretty version of the user struct
-func (u User) String() string {
-	return fmt.Sprintf(
-		"Username: %s\nFirstname: %s\nLastName: %s\nCloudProviders: %v\nEnabled: %v\n",
-		u.Username, u.FirstName, u.LastName, u.CloudProviders, u.Enabled,
-	)
+type Component1 struct {
+	ID     string
+	Config Component1Config
 }
 
-func main() {
-	// Get a handle to the hclparser
-	parser := hclparse.NewParser()
+func (c *Component1) Update(ID string, config ComponentConfig) {
+	c.ID = ID
+	c.Config = config.(Component1Config)
+}
 
-	// Specify file to parse.
-	// Alternative is directly pass in a []byte  containing the contents of an `hcl` configuration
-	// using parser.ParseHCL([]byte, string)
-	file, diags := parser.ParseHCLFile("./config.hcl")
+func (c *Component1) GetID() string {
+	return c.ID
+}
 
-	// diags is represents an error value containing diagnostic information. Utilize the
-	// HasErrors() func to check if any errors exist. These parse methods return hcl.Diagnostics
-	// instead of `error` values. However, the hcl.Diagnostics implements the `error` interface
-	// so you can use them for passing up error values.
-	if diags.HasErrors() {
-		log.Fatal(diags)
-	}
+func (c *Component1) Run() {
+	fmt.Printf("Component1: ID -> %s, Config -> %v\n", c.ID, c.Config)
+}
 
-	// The `gohcl` package contains high-level functions for decoding `hcl` into native
-	// go values. The second parameter contains the Evaluation Context which is currently nil.
-	// That will be demonstrated in another example.
-	var config Configuration
-	var ctx = hcl.EvalContext{
-		Variables: map[string]cty.Value{
-			"foo": cty.StringVal("bar"),
-			"bar": cty.StringVal("baz"),
-		},
-	}
-	confDiags := gohcl.DecodeBody(file.Body, &ctx, &config)
-
-	if confDiags.HasErrors() {
-		log.Fatal(confDiags)
-	}
-
-	fmt.Println(config.Version)
-	fmt.Println(config.Tags)
-
-	for _, user := range config.Users {
-		fmt.Println(user)
-	}
-
-	// sub-config with cty tags
-	// obj, _ := gohcl.ImpliedBodySchema(config)
-	tp, err := gocty.ImpliedType(config)
+func (c *Component1) Exports() map[string]cty.Value {
+	enabledType, err := gocty.ImpliedType(c.Config.Enabled)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	val, _ := gocty.ToCtyValue(config, tp)
-	var postCtx = hcl.EvalContext{
-		Variables: map[string]cty.Value{
-			"config": val,
+	val, _ := gocty.ToCtyValue(c.Config.Enabled, enabledType)
+	return map[string]cty.Value{
+		"enabled": val,
+	}
+}
+
+var componentNodeTypes = map[string]ComponentNode{
+	"component1": {
+		ID:            "component1",
+		ComponentType: reflect.TypeOf(Component1{}),
+		ConfigType:    reflect.TypeOf(Component1Config{}),
+		Schema: hcl.BodySchema{
+			Blocks: []hcl.BlockHeaderSchema{
+				{
+					Type:       "component1",
+					LabelNames: []string{"name"},
+				},
+			},
 		},
+	},
+}
+
+var components = []Component{}
+
+var configSchema hcl.BodySchema
+
+func init() {
+	for _, component := range componentNodeTypes {
+		configSchema.Blocks = append(configSchema.Blocks, component.Schema.Blocks...)
+	}
+}
+
+func main() {
+	parser := hclparse.NewParser()
+
+	file, diags := parser.ParseHCL([]byte(testConfig), "testConfig")
+	if diags.HasErrors() {
+		log.Fatal(diags)
 	}
 
-	var postConfig PostConfiguration
-	confDiags = gohcl.DecodeBody(config.Remain, &postCtx, &postConfig)
+	var config Configuration
+
+	confDiags := gohcl.DecodeBody(file.Body, nil, &config)
 	if confDiags.HasErrors() {
 		log.Fatal(confDiags)
 	}
 
-	fmt.Println(postConfig.Pepe)
+	content, remainDiags := config.Remain.Content(&configSchema)
+	diags = diags.Extend(remainDiags)
+	if diags.HasErrors() {
+		log.Fatal(diags.Errs())
+	}
+
+	var ctx = hcl.EvalContext{
+		Variables: map[string]cty.Value{},
+	}
+	for _, block := range content.Blocks {
+		componentNode, ok := componentNodeTypes[block.Type]
+		if !ok {
+			log.Fatalf("unknown component type %q", block.Type)
+		}
+
+		if len(block.Labels) != 1 {
+			log.Fatalf("expected exactly one label for component %q", block.Type)
+		}
+
+		component := reflect.New(componentNode.ComponentType).Interface().(Component)
+		componentConfig := reflect.New(componentNode.ConfigType).Interface()
+
+		if err := gohcl.DecodeBody(block.Body, &ctx, componentConfig); err != nil {
+			log.Fatal(err)
+		}
+
+		component.Update(block.Labels[0], reflect.ValueOf(componentConfig).Elem().Interface())
+		components = append(components, component)
+
+		for name, attr := range component.Exports() {
+			ctx.Variables[fmt.Sprintf("%v_%v_exports_%v", componentNode.ID, component.GetID(), name)] = attr
+		}
+	}
+
+	for _, component := range components {
+		component.Run()
+	}
 }
